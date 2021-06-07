40033f73b36d17d9e5fa49735922c766cc1fc289
João Vitor Villas <jvvillasb@gmail.com>
2021-06-07 01:38:55 -0300

    Trabalho Final bds04

---
+++ src/main/java/com/devsuperior/bds04/services/exceptions/DataBaseExceptions.java
@@ -0,0 +1,10 @@
+package com.devsuperior.bds04.services.exceptions;
+
+public class DataBaseExceptions extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public DataBaseExceptions(String msg) {
+		super(msg);
+	}
+}
